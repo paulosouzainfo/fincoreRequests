a6c16791014a01b8d9e717f010a2d4e97ccc0acb
paulosouza.info <paulosouza.info@gmail.com>
2019-06-25 10:05:42 -0300

    validação de testes para login administrativo

--- examples/AdministrativeLogin.php
+++ examples/AdministrativeLogin.php
@@ -2,8 +2,15 @@
 require_once './vendor/autoload.php';
 
 $shiftScript = array_shift($argv);
-
 list($email, $password) = $argv;
 
+$println = function($text, $dump = false) {
+  if($dump) print_r($dump).PHP_EOL;
+  else echo $text.PHP_EOL;
+};
+
 $action = new \Fincore\Access();
-print_r($action->administrative($email, $password)).PHP_EOL;
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) $println('Você precisa informar um e-mail válido.');
+else if(empty($password)) $println('Para processamento do seu login é necessário o envio de uma senha.');
+else $println($action->administrative($email, $password), true);
