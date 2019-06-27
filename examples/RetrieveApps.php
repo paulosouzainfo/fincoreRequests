737d015c0488b1d983b53510fddc449d83604371
kakaroto developer <kakaroto1500@gmail.com>
2019-06-27 17:13:06 -0300

    removendo os espaços de retrieveapps

--- examples/RetrieveApps.php
+++ examples/RetrieveApps.php
@@ -2,8 +2,6 @@
 require_once './vendor/autoload.php';
 
 $shiftScript = array_shift($argv);
-list($IdApp) = $argv;
-
 $request = new \Fincore\Administrative();
-$request->println($request->RetrieveApps($IdApp), true);
+$request->println($request->RetrieveApps($argv[0]), true);
 
