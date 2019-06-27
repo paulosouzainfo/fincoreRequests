97a017d73a5f9107c0323ead38eb0d540878e4d8
kakaroto developer <kakaroto1500@gmail.com>
2019-06-27 10:00:33 -0300

    Inserindos os metodos restante do Apps

--- src/Administrative.php
+++ src/Administrative.php
@@ -6,6 +6,38 @@ class Administrative extends \Fincore\Requests {
     parent::__construct();
   }
 
+  public UpdatingApps ($url,$dsn,$IdApp){
+
+    $request = [
+      'path' => '/apps/'.$IdApp.'?force=1',
+      'data' => [
+          'url' => $url,
+          'dsn' => $dsn
+      ]
+    ];
+    return  $this->put($this->buildQuery($request));
+ 
+  }
+
+  public function DisableApps($IdApp)
+  {
+    $request = [
+      'path' => '/apps/'.$IdApp
+    ];
+
+    return  $this->delete($this->buildQuery($request));
+  }
+
+
+public function ListApps()
+  {
+    $request = [
+      'path' => '/apps'
+    ];
+
+    return  $this->get($this->buildQuery($request));
+  }
+
   public function NewApps($url,$dsn)
   {
     $request = [
@@ -19,21 +51,26 @@ class Administrative extends \Fincore\Requests {
     return  $this->post($this->buildQuery($request));
   }
 
-  public function ListApps()
+  public function ReactivatingApps($url,$dsn)
   {
     $request = [
-      'path' => '/apps'
+      'path' => '/apps',
+         'data' => [
+          'url' => $url,
+          'dsn' => $dsn
+      ]
     ];
 
-    return  $this->get($this->buildQuery($request));
+    return  $this->post($this->buildQuery($request));
   }
 
-public function DisableApps($IdApp)
+  public function RetrieveApps($IdApp)
   {
     $request = [
       'path' => '/apps/'.$IdApp
     ];
 
-    return  $this->delete($this->buildQuery($request));
+    return  $this->get($this->buildQuery($request));
   }
+
 }
