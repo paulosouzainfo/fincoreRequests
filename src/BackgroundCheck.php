d05e8a173358e46b0380304392fac47b19209b44
paulosouza.info <paulosouza.info@gmail.com>
2019-06-28 18:01:21 -0300

    regras para background check

--- src/BackgroundCheck.php
+++ src/BackgroundCheck.php
@@ -5,4 +5,49 @@ class BackgroundCheck extends \Fincore\Requests {
   public function __construct() {
     parent::__construct();
   }
+  
+  public function question(int $document): objetct {
+    $request = [
+      'path' => '/_/outsourcing/questions',
+      'queryString' => [
+        'document' => $document
+      ]
+    ];
+
+    return  $this->get($this->buildQuery($request));
+  }
+  
+  public function answers(string $questionTicket, array $answers): objetct {
+    $request = [
+      'path' => "/_/outsourcing/background-check/answer-me/{$ticket}",
+      'data' => $answers
+    ];
+
+    return  $this->post($this->buildQuery($request));
+  }
+  
+  public function documentsOCR(string $imageURL, string $type, string $side): objetct {
+    $request = [
+      'path' => "/_/outsourcing/verify-id",
+      'data' => [
+        'image' => $imageURL,
+        'type' => $type,
+        'side' => $side
+      ]
+    ];
+
+    return  $this->post($this->buildQuery($request));
+  }
+  
+  public function facematch(string $documentURL, string $selfieURL): objetct {
+    $request = [
+      'path' => "/_/outsourcing/background-check/facematch",
+      'data' => [
+        'document' => $documentURL,
+        'selfie' => $selfieURL
+      ]
+    ];
+
+    return  $this->post($this->buildQuery($request));
+  }
 }
