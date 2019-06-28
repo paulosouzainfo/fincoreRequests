3a7cbd760b6d35b314326b2266ba991a9635e179
paulosouza.info <paulosouza.info@gmail.com>
2019-06-28 16:20:19 -0300

    refactoring de testes para login de aplicações e administrativo

--- examples/AdministrativeLogin.php
+++ examples/AdministrativeLogin.php
@@ -1,11 +1,6 @@
 <?php
 require_once './vendor/autoload.php';
 
-$shiftScript = array_shift($argv);
-list($email, $password) = $argv;
-
 $request = new \Fincore\Access();
 
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)) $request->println('Você precisa informar um e-mail válido.');
-else if(empty($password)) $request->println('Para processamento do seu login é necessário o envio de uma senha.');
-else $request->println($request->administrative($email, $password), true);
+$request->println($request->administrative(), true);
--- examples/AppsLogin.php
+++ examples/AppsLogin.php
@@ -1,9 +1,5 @@
 <?php
 require_once './vendor/autoload.php';
 
-$shiftScript = array_shift($argv);
-
-list($secret, $userID, $token) = $argv;
-
 $action = new \Fincore\Access();
-print_r($action->apps($secret, $userID, $token)).PHP_EOL;
+print_r($action->apps()).PHP_EOL;
