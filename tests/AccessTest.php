27303b90838f5ffbaa88ed89abb3ec8eb00e5d47
paulosouza.info <paulosouza.info@gmail.com>
2019-07-02 11:42:42 -0300

    testes de input e output da api separados para seguir como padrão

--- tests/AccessTest.php
+++ tests/AccessTest.php
@@ -20,11 +20,16 @@ final class AccessTest extends \PHPUnit\Framework\TestCase {
 	/**
 	* @dataProvider senhaAleatoria
 	*/
-	public function testVerificarAcessoAdministrativo($email, $password): void {
+	public function testInputAdministrativo($email, $password): void {
 		$this->assertEquals($email, filter_var($email, FILTER_VALIDATE_EMAIL));
 		$this->assertTrue(strlen($password) >= 6);
-		
-		$request = $this->access->administrative($email,$password);
+	}
+	
+	/**
+	* @dataProvider senhaAleatoria
+	*/
+	public function testResponseAdministrativo($email, $password): void {
+		$request = $this->access->administrative($email, $password);
 		
 		$this->assertEquals(200, $request->http_status);
 		$this->assertEquals($email, $request->response->email);
