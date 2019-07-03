0888d1fa06e31e271aaf48c5c961b20a9db3761a
kakaroto developer <kakaroto1500@gmail.com>
2019-07-03 14:08:30 -0300

    envio de teste da classe account

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -11,9 +11,23 @@ final class AccountTest extends \PHPUnit\Framework\TestCase {
 		$this->Account = new \Fincore\Account();
    }
 
+   public function InfoPerfil(): array {
+   	return array(
+   		      array(
+                'nickName'=>getenv('NICKNAME')
+            ));
+      }
+  /**
+  * @dataProvider InfoPerfil
+  */
+
+   public function testUpdatingRegistration($data) {
+   	$request =$this->Account->UpdatingRegistration($data);
+    	$this->assertEquals(200, $request->http_status);
+   }
+
    public function testRecoveringData(): void {
    	$request = $this->Account->RecoveringData();
    	$this->assertEquals(200, $request->http_status);
 	}
-
-}
+ }
