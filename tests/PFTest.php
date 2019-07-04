d5670159408976be19b450eaaca5682d491a2407
kakaroto developer <kakaroto1500@gmail.com>
2019-07-04 16:21:40 -0300

    inserindo novos teste na classe pf

--- tests/PFTest.php
+++ tests/PFTest.php
@@ -40,8 +40,58 @@ final class PFTest extends \PHPUnit\Framework\TestCase {
     $request =$this->PF->ClassAdvice($cpf);
     $this->assertEquals(200, $request->http_status);
   }
+   /**
+  * @dataProvider CPF
+  */
 
+  public function testPublicPprofessions($cpf): void {
+    $request =$this->PF->PublicPprofessions($cpf);
+    $this->assertEquals(200, $request->http_status);  
+    $this->assertEquals($cpf, $request->response->document);
+   
+  }
+  /**
+  * @dataProvider CPF
+  */
 
+  public function testUniversitySstudents($cpf): void {
+    $request =$this->PF->UniversitySstudents($cpf); 
+    $this->assertEquals(200, $request->http_status); 
+  }
+  /**
+  * @dataProvider CPF
+  */
+
+  public function testPersonsDomains($cpf): void {
+    $request =$this->PF->PersonsDomains($cpf); 
+     $this->assertEquals(200, $request->http_status); 
+  }
+
+   /**
+  * @dataProvider CPF
+  */
+
+  public function testPersonsAddresses($cpf): void {
+   $request =$this->PF->PersonsAddresses($cpf); 
+   var_dump($request);
+   $this->assertEquals(200, $request->http_status); 
+   
+  }
+
+  /**
+  * @dataProvider CPF
+  */
+
+  public function testPersonsMediaExposure($cpf): void {
+    $request =$this->PF->PersonsMediaExposure($cpf); 
+     $this->assertEquals(200, $request->http_status); 
+
+  }
+
+  /*public function testPersonsEmails($cpf): void {
+    $request =$this->PF->PersonsEmails($cpf); 
+    $this->assertEquals(200, $request->http_status); 
+  }*/
 
   
 
