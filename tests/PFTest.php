6d5afe8bca28062d611f7756f5e6ed2c1634725b
kakaroto developer <kakaroto1500@gmail.com>
2019-07-04 14:25:27 -0300

    Inserindo novos  teste na classe PF

--- tests/PFTest.php
+++ tests/PFTest.php
@@ -10,7 +10,6 @@ final class PFTest extends \PHPUnit\Framework\TestCase {
   protected function setup(): void {
     $this->PF = new \Fincore\PF();
    }
-
   public function CPF(): array {
     return [
       [getenv('CPF')]
@@ -19,11 +18,27 @@ final class PFTest extends \PHPUnit\Framework\TestCase {
  /**
   * @dataProvider CPF
   */
-
    public function testOnlineAds($cpf): void {
     $request =$this->PF->OnlineAds($cpf);
-    var_dump($request);
+    $this->assertEquals(200, $request->http_status);   
     
+  }
+  /**
+  * @dataProvider CPF
+  */
+  public function testBasicCadastral($cpf): void {
+    $request =$this->PF->BasicCadastral($cpf);
+    $this->assertEquals(200, $request->http_status);
+    $this->assertEquals($cpf, $request->response->document);
+ 
+  }
+   /**
+  * @dataProvider CPF
+  */
+
+  public function testClassAdvice($cpf): void {
+    $request =$this->PF->ClassAdvice($cpf);
+    $this->assertEquals(200, $request->http_status);
   }
 
 
