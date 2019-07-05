82595544686370ea521d2f725f10a6d031dd8b94
kakaroto developer <kakaroto1500@gmail.com>
2019-07-05 17:35:08 -0300

    Envio de Teste da Classe telefone

---
+++ tests/PhonesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fincore\Test;
+
+$dotenv = \Dotenv\Dotenv::create('./');
+$dotenv->load();
+
+final class PhonesTest extends \PHPUnit\Framework\TestCase
+{
+    private $Phones;
+    protected function setup(): void
+    {
+        $this->Phones = new \Fincore\Phones();
+    }
+    public function phone(): array
+    {
+        return [
+            [getenv('PHONE')],
+        ];
+    }
+    /**
+     * @dataProvider phone
+     */
+    /*public function testPhonesHistory($phone)
+    {
+        $request = $this->Phones->PhonesHistory($phone);
+        $this->assertEquals(200, $request->http_status);
+    }*/
+    /**
+     * @dataProvider phone
+     */
+    /*public function testPhoneData($phone)
+    {
+        $request = $this->Phones->PhoneData($phone);
+        $this->assertEquals(200, $request->http_status);
+
+    }*/
+}
