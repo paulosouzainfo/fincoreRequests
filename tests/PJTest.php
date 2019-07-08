9478ca4bdc45177e16a8d6401610d30e4056a13e
kakaroto developer <kakaroto1500@gmail.com>
2019-07-08 09:32:34 -0300

    Envio de Teste de PJ

---
+++ tests/PJTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Fincore\Test;
+
+$dotenv = \Dotenv\Dotenv::create('./');
+$dotenv->load();
+
+final class PJTest extends \PHPUnit\Framework\TestCase
+{
+
+    private $PJ;
+    protected function setup(): void
+    {
+        $this->PJ = new \Fincore\Phones();
+    }
+    public function Cnpj(): array
+    {
+        return [
+            [getenv('CNPJ')],
+        ];
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+
+    public function testads($cnpj): void
+    {
+
+        $request = $this->PJ->ads($cnpj);
+        $this->assertEquals(200, $request->http_status);
+
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testbasic($cnpj): void
+    {
+        $request = $this->PJ->basic($cnpj);
+        $this->assertEquals(200, $request->http_status);
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testdomains($cnpj): void
+    {
+        $request = $this->PJ->domains($cnpj);
+        $this->assertEquals(200, $request->http_status);
+
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testemails($cnpj): void
+    {
+        $request = $this->PJ->emails($cnpj);
+        $this->assertEquals(200, $request->http_status);
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testmediaExposure($cnpj): void
+    {
+        $request = $this->PJ->mediaExposure($cnpj);
+        $this->assertEquals(200, $request->http_status);
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testactivityIndicators($cnpj): void
+    {
+        $request = $this->PJ->activityIndicators($cnpj);
+        $this->assertEquals(200, $request->http_status);
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testrelationships($cnpj): void
+    {
+        $request = $this->PJ->relationships($cnpj);
+        $this->assertEquals(200, $request->http_status);
+    }
+    /**
+     * @dataProvider Cnpj
+     */
+    public function testphones($cnpj): void
+    {
+        $request = $this->PJ->relationships($cnpj);
+        $this->assertEquals(200, $request->http_status);
+    }
+}
