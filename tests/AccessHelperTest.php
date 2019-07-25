62194313fe0ae4a661c54221c57118949c367d37
kakaroto developer <kakaroto1500@gmail.com>
2019-07-25 18:52:47 -0300

    Atualizando os testes em Acess Helper

--- tests/AccessHelperTest.php
+++ tests/AccessHelperTest.php
@@ -4,23 +4,28 @@ namespace Fincore\Test;
 $dotenv = \Dotenv\Dotenv::create('./');
 $dotenv->load();
 
-final class AccessHelperTest extends \PHPUnit\Framework\TestCase {
-	private $access;
-	
-	protected function setup(): void {
-		$this->access = new \Fincore\AccessHelper();
-   }
+final class AccessHelperTest extends \PHPUnit\Framework\TestCase
+{
+    private $access;
 
-  public function InfoEmail(): array {
-    return [
-      [getenv('EMAIL')]
-    ];
-  }
-  /**
-  * @dataProvider InfoEmail
-  */
-   public function testforgot($email) {
-    $request = $this->access->forgot($email);
-    $this->assertEquals(200, $request->http_status);
-   }
+    protected function setup(): void
+    {
+        $this->access = new \Fincore\AccessHelper();
+    }
+
+    public function InfoEmail(): array
+    {
+        return [
+            [getenv('EMAIL')],
+        ];
+    }
+    /**
+     * @dataProvider InfoEmail
+     */
+    public function testforgot($email)
+    {
+        $request = $this->access->forgot($email);
+        $this->assertEquals(true, strpos($request->response, $email));
+        $this->assertEquals(200, $request->http_status);
+    }
 }
