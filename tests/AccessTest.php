803cdcb6563ad0ac3663ae9298b195d3e1e39f69
kakaroto developer <kakaroto1500@gmail.com>
2019-07-25 17:33:13 -0300

    Atualizando teste Acess test

--- tests/AccessTest.php
+++ tests/AccessTest.php
@@ -28,33 +28,46 @@ final class AccessTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($email, filter_var($email, FILTER_VALIDATE_EMAIL));
         $this->assertTrue(strlen($password) >= 6);
     }
-
     /**
      * @dataProvider senhaAleatoria
      */
     public function testResponseAdministrativo($email, $password): void
     {
-        $request = $this->access->Administrative($email, $password);
-
+        $request             = $this->access->Administrative($email, $password);
+        $arrayAdministrativo = (array) $request->response;
+        $this->assertArrayHasKey('email', $arrayAdministrativo);
+        $this->assertArrayHasKey('status', $arrayAdministrativo);
+        $this->assertArrayHasKey('role', $arrayAdministrativo);
+        $this->assertArrayHasKey('zodiacSign', $arrayAdministrativo);
+        $this->assertArrayHasKey('chineseSign', $arrayAdministrativo);
+        $this->assertArrayHasKey('age', $arrayAdministrativo);
+        $this->assertArrayHasKey('bornAt', $arrayAdministrativo);
+        $this->assertArrayHasKey('createdAt', $arrayAdministrativo);
+        $this->assertEquals($email, $arrayAdministrativo['email']);
+        $this->assertEquals('active', $arrayAdministrativo['status']);
+        $this->assertEquals('admin', $arrayAdministrativo['role']);
+        $this->assertEquals('CANCER', $arrayAdministrativo['zodiacSign']);
+        $this->assertEquals('Tiger', $arrayAdministrativo['chineseSign']);
+        $this->assertEquals('44', $arrayAdministrativo['age']);
+        $this->assertEquals('1974-07-21', date("Y-m-d", strtotime($arrayAdministrativo['bornAt'])));
+        $this->assertEquals('2019-06-24', date("Y-m-d", strtotime($arrayAdministrativo['createdAt'])));
         $this->assertEquals(200, $request->http_status);
-        $this->assertEquals($email, $request->response->email);
-    }
 
+    }
     public function InfoDadosApps(): array
     {
         return [
             [getenv('SECRET'), getenv('USERID'), getenv('TOKEN')],
         ];
     }
-
     /**
      * @dataProvider InfoDadosApps
      */
-
     public function testApps($secret, $userID, $token): void
     {
         $request = $this->access->Apps($secret, $userID, $token);
         $this->assertEquals(200, $request->http_status);
         $this->assertEquals($userID, $request->response->user_id);
+        $this->assertEquals($token, $request->response->token);
     }
 }
