1902bae1a02049f59f103d6392053afaef74f4d3
kakaroto developer <kakaroto1500@gmail.com>
2019-07-29 16:18:50 -0300

    atualizando teste em utilities

--- tests/UtilitiesTest.php
+++ tests/UtilitiesTest.php
@@ -14,8 +14,18 @@ final class UtilitiesTest extends \PHPUnit\Framework\TestCase
     }
     public function testJsonToXls()
     {
-        $data    = ['a' => 1, 'b' => 1];
-        $request = $this->Utilities->JsonToXls($data);        
+        $data       = ['a' => 1, 'b' => 1];
+        $request    = $this->Utilities->JsonToXls($data);
+        $file_path  = str_replace('File criado com sucesso em ', '', $request->response);
+        list($pasta, $subpasta) = explode('/', $file_path);
+        $this->assertDirectoryExists($pasta);
+        $this->assertDirectoryExists($pasta . '/' . $subpasta);
+        $this->assertFileExists($file_path);
+        $this->assertFileIsReadable($file_path);
+        $this->assertFileIsWritable($file_path);
         $this->assertEquals(200, $request->http_status);
+        $this->assertEquals(true, unlink($file_path));
+        $this->assertEquals(true, rmdir($pasta . '/' . $subpasta));
+        $this->assertEquals(true, rmdir($pasta));
     }
 }
